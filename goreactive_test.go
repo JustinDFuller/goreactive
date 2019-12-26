579eca43539619dc9a5e2683c99ca44070df08da
Justin Fuller <justindanielfuller@gmail.com>
2019-12-26 12:57:05 -0500

    Component test

--- goreactive_test.go
+++ goreactive_test.go
@@ -9,29 +9,30 @@ import (
 )
 
 const (
-	helloWorldExpected = "<p>Hello World</p>"
-	childrenExpected   = "<ul><li>one</li><li>two</li></ul>"
+	ExpectedHelloWorld     = "<p>Hello World</p>"
+	ExpectedChildren       = "<ul><li>one</li><li>two</li></ul>"
+	ExpectedChildComponent = "<div>Child component props!</div>"
 )
 
 // helloWorld tests the very basic mechanisms of rendering html.
 type helloWorld struct{}
 
-func (_ helloWorld) Render() node.Node {
+func (_ *helloWorld) Render() node.Node {
 	return node.New(tags.P, node.Children(node.Text("Hello World")))
 }
 
 func TestRenderToString(t *testing.T) {
 	result := RenderToString(&helloWorld{})
 
-	if result != helloWorldExpected {
-		t.Fatalf("Expected '%s'. Got '%s'.", helloWorldExpected, result)
+	if result != ExpectedHelloWorld {
+		t.Fatalf("Expected '%s'. Got '%s'.", ExpectedHelloWorld, result)
 	}
 }
 
 // withChildren tests the mechanisms for rendering child components.
 type withChildren struct{}
 
-func (_ withChildren) Render() node.Node {
+func (_ *withChildren) Render() node.Node {
 	return node.New(
 		tags.Ul,
 		node.Children(
@@ -50,8 +51,8 @@ func (_ withChildren) Render() node.Node {
 func TestChildrenRenderToString(t *testing.T) {
 	result := RenderToString(&withChildren{})
 
-	if result != childrenExpected {
-		t.Fatalf("Expected '%s'. Got '%s'.", childrenExpected, result)
+	if result != ExpectedChildren {
+		t.Fatalf("Expected '%s'. Got '%s'.", ExpectedChildren, result)
 	}
 }
 
@@ -68,10 +69,18 @@ func (c childComponent) Render() node.Node {
 
 type withChildComponent struct{}
 
-func (_ withChildComponent) Render() node.Node {
+func (_ *withChildComponent) Render() node.Node {
 	return component.New(
 		childComponent{
 			text: "Child component props!",
 		},
 	)
+}
+
+func TestChildComponent(t *testing.T) {
+	result := RenderToString(&withChildComponent{})
+
+	if result != ExpectedChildComponent {
+		t.Fatalf("Expected '%s'. Got '%s'.", ExpectedChildComponent, result)
+	}
 }
